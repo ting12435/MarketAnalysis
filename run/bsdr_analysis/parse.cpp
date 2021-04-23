1b677e289a8c20fe77844bc4789af5d7d295dd75
timcheng <tim.cheng@witstech.com.tw>
2021-04-23 15:05:05 +0800

    one_stock_issuers_distribution

--- run/bsdr_analysis/parse.cpp
+++ run/bsdr_analysis/parse.cpp
@@ -8,15 +8,19 @@
 #include "util/util.h"
 
 struct var {
+	std::string parse_type;
 	Date *d1;
 	Date *d2;
+	std::string issuer;
+	std::string stock;
 } g_var;
 
 void parse();
-void parse_trade_detail();
+void parse_one_issuer_bsnet();
 void parse_one_issuer();
 void parse_issuers();
 void parse_debug(bsdr_data_t*);
+void parse_one_stock_issuers_distribution();
 
 void init_g_var() {
 	memset(&g_var, 0, sizeof(g_var));
@@ -29,8 +33,11 @@ bool check_g_var() {
 }
 
 static struct option long_options[] = {
-	{"d1", required_argument, NULL, 0},
-	{"d2", required_argument, NULL, 1}
+	{"type", required_argument, NULL, 0},
+	{"d1", required_argument, NULL, 1},
+	{"d2", required_argument, NULL, 2},
+	{"issuer", required_argument, NULL, 3},
+	{"stock", required_argument, NULL, 4}
 };
 
 int main(int argc, char *argv[]) {
@@ -44,10 +51,19 @@ int main(int argc, char *argv[]) {
 	while ((c = getopt_long (argc, argv, "", long_options, NULL)) != -1) {
 		switch (c) {
 			case 0:
-				g_var.d1 = new Date(optarg);
+				g_var.parse_type = optarg;
 				break;
 			case 1:
+				g_var.d1 = new Date(optarg);
+				break;
+			case 2:
 				g_var.d2 = new Date(optarg);
+				break;
+			case 3:
+				g_var.issuer = optarg;
+				break;
+			case 4:
+				g_var.stock = optarg;
 				break;
 			default:
 				goto usage_error;
@@ -56,30 +72,26 @@ int main(int argc, char *argv[]) {
 
 	assert(check_g_var());
 
-	// Date *st_date, *ed_date;
-	// bsdr_data_t *bsdr_data;
-
-	// st_date = new Date("2021-02-17");
-	// ed_date = new Date("2021-02-17");
-
-	// bsdr_data = new bsdr_data_t;
-	// BSDR::get_data(bsdr_data, st_date, ed_date, Market::ALL);
-
-	
-	// parse_debug(bsdr_data);
-
-	// parse_one_issuer();
-	parse_trade_detail();
-	// parse_issuers();
-	// parse();
+	if (g_var.parse_type == "debug")
+		parse_debug(bsdr_data);
+	else if (g_var.parse_type == "one_issuer")
+		parse_one_issuer();
+	else if (g_var.parse_type == "one_issuer_bsnet")
+		parse_one_issuer_bsnet();
+	else if (g_var.parse_type == "issuers")
+		parse_issuers();
+	else if (g_var.parse_type == "test")
+		parse();
+	else if (g_var.parse_type == "one_stock_issuers_distribution")
+		parse_one_stock_issuers_distribution();
 
 	return 0;
 
 	usage_error:
 	fprintf(stderr, "Usage: %s\n", argv[0]);
-	fprintf(stderr, "%9s [--d1] [--d2]\n", " ");
+	fprintf(stderr, "%9s [--type] [--d1] [--d2] [--issuer]\n", " ");
 	fprintf(stderr, "\ne.g.\n");
-	fprintf(stderr, "taskset -c 5 %s --d1 2021-04-20 --d2 2021-04-21\n", argv[0]);
+	fprintf(stderr, "taskset -c 5 %s --type one_issuer_bsnet --d1 2021-04-20 --d2 2021-04-21 --issuer 5850\n", argv[0]);
 	return EXIT_FAILURE;
 }
 
@@ -102,7 +114,7 @@ void parse() {
 	}
 }
 
-void parse_trade_detail() {
+void parse_one_issuer_bsnet() {
 	struct trade_detail {
 		int b_lot;
 		int s_lot;
@@ -110,7 +122,10 @@ void parse_trade_detail() {
 		long s_amount;
 	};
 
-	const std::string issuer_name = "5850";
+	// const std::string issuer_name = "5850";
+	std::string issuer_name = g_var.issuer;
+
+
 	// bsdr_data_t bsdr_data = BSDR::get_data(Date("2021-01-01"), Date("2021-02-18"), Market::ALL);
 	// std::map<std::string, std::map<std::string, std::map<std::string, struct trade_detail>>> d;  // <K:date_str, <K: issuer, V:<K: stock, V: detail>>>
 
@@ -271,6 +286,36 @@ void parse_debug(bsdr_data_t *bsdr_data) {
 		}
 	}
 	my_file.close();
+}
+
+void parse_one_stock_issuers_distribution() {
+	struct trade_detail {
+		int b_lot;
+		int s_lot;
+		long b_amount;
+		long s_amount;
+	};
+
+	bsdr_date_issuer_stock_t d = BSDR::get_analysis_data_date_issuer_stock(*g_var.d1, *g_var.d2, Market::ALL);
+	std::map<std::string, struct trade_detail> m;  // K:issuer
+
+	for (const auto &date_d: d) {
+		for (const auto &issuer_d: date_d.second) {
+			for (const auto &stock_d: issuer_d.second) {
+				if (stock_d.first == g_var.stock) {
+					m[issuer_d.first].b_lot += stock_d.second.b_lot;
+					m[issuer_d.first].s_lot += stock_d.second.s_lot;
+					m[issuer_d.first].b_amount += stock_d.second.b_amount;
+					m[issuer_d.first].s_amount += stock_d.second.s_amount;
+				}
+			}
+		}
+	}
+
+	// output
+	for (const auto &issuer_d: m) {
+		std::cout << "=\"" << issuer_d.first << "\"," << issuer_d.second.b_lot << "," << issuer_d.second.s_lot << "," << issuer_d.second.b_lot - issuer_d.second.s_lot << "," << issuer_d.second.b_amount - issuer_d.second.s_amount << std::endl;
+	}
 }
 
 
