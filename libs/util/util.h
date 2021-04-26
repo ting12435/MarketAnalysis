29b7cdedca3e2a0828e09c341dbf1ee80be85afd
timcheng <tim.cheng@witstech.com.tw>
2021-04-26 16:33:23 +0800

    md_pcap

--- libs/util/util.h
+++ libs/util/util.h
@@ -11,6 +11,8 @@
 #include <fstream>
 #include <sstream>
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define OUTPUT(msg) std::cout << msg << std::endl;
 
 class Date {
