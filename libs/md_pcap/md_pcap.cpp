dc7ff3d65c56f2cdc3ba978f1607134c182e0e11
timcheng <tim.cheng@witstech.com.tw>
2021-04-29 16:51:27 +0800

    md_pcap

--- libs/md_pcap/md_pcap.cpp
+++ libs/md_pcap/md_pcap.cpp
@@ -50,8 +50,9 @@ struct md* OneDayPcap::get_md() {
 					break;
 			}
 		}
+
 		this->record_data_st_ptr = this->record_data + 42;
-		this->record_data_ed_ptr = this->record_data_st_ptr + record_data_len;
+		this->record_data_ed_ptr = this->record_data_st_ptr + record_data_len - 4;
 	}
 // print_hexdump(this->record_data, record_data_len);
 // printf("record_data_len=%d\n", record_data_len);
