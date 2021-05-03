fb1417ba04b44f50d4f0427105943bb05fda0344
timcheng <tim.cheng@witstech.com.tw>
2021-05-03 18:48:47 +0800

    md_pcap

--- run/md_pcap_analysis/main.cpp
+++ run/md_pcap_analysis/main.cpp
@@ -280,10 +280,12 @@ void large_amount() {
 						info_ptr->accm_trade_lot += md.trade_lt;
 
 					if (md.feedcode == "2330  ") {
-						std::cout << md.trade_lt << " " << md.accm_trade_lot << " " << info_ptr->accm_trade_lot << std::endl;
+						// std::cout << md.trade_lt << " " << md.accm_trade_lot << " " << info_ptr->accm_trade_lot << std::endl;
 
-					// 	// if (md.match_time_sec > 90000)
-					// 	// 	print_hexdump((char*)frame, md.md_len);
+						if (md.match_time_sec > 90000) {
+							// print_hexdump((char*)frame, md.md_len);
+							md.print_detail();
+						}
 					}
 
 				}
