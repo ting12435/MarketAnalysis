b82ec555e67acfb1defb9f8f7797dc0fdf1b787e
timcheng <tim.cheng@witstech.com.tw>
2021-05-03 14:12:02 +0800

    md_pcap

--- run/md_pcap_analysis/main.cpp
+++ run/md_pcap_analysis/main.cpp
@@ -224,7 +224,7 @@ void uplimit() {
 					auto px2 = cur_iter->second[stock_d.first].highest_px;
 					if (px1 > 0) {
 						denominator++;
-						// std::cout << prv_iter->first << " " << cur_iter->first << " " << stock_d.first << " " << px1 << " " << px2 << " " << (px2 >= px1) << std::endl;
+						std::cout << prv_iter->first << " " << cur_iter->first << " " << stock_d.first << " " << px1 << " " << px2 << " " << (px2 >= px1) << std::endl;
 						if (px2 >= px1) {
 							fraction++;
 						}
