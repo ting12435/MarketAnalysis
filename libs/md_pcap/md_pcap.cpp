42e4856577fafe79b6696b2c46800437fc6f1f59
timcheng <tim.cheng@witstech.com.tw>
2021-05-05 15:14:21 +0800

    md

--- libs/md_pcap/md_pcap.cpp
+++ libs/md_pcap/md_pcap.cpp
@@ -18,7 +18,7 @@ OneDayPcap::OneDayPcap(Date d) {
 	this->cur_pcap_idx = 0;
 	this->cur_pcap_file = nullptr;
 	this->date_str = this->date.date_str.substr(0, 4) + this->date.date_str.substr(5, 2) + this->date.date_str.substr(8, 2);
-	if (this->date_str == "20210504")
+	if (this->date_str == "20210505")
 		this->date_folder = pcap_folder + this->date_str + "_1330";
 	else
 		this->date_folder = pcap_folder + this->date_str;
