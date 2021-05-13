733bb698c318dfd78fdeb254cb751dc1886a5af6
timcheng <tim.cheng@witstech.com.tw>
2021-05-13 14:35:03 +0800

    md

--- run/md_pcap_analysis/main.cpp
+++ run/md_pcap_analysis/main.cpp
@@ -408,12 +408,14 @@ void large_amount() {
 
 void interactive() {
 
-	struct priv_md {
-		int bid_px[5];
-		int bid_lt[5];
-		int ask_px[5];
-		int ask_lt[5];
-	} priv;
+	// struct priv_md {
+	// 	int bid_px[5];
+	// 	int bid_lt[5];
+	// 	int ask_px[5];
+	// 	int ask_lt[5];
+	// } priv;
+
+	std::map<int, int> bid_pxlt_map, ask_pxlt_map;
 
 	struct md *frame;
 	MD md;
@@ -454,11 +456,11 @@ void interactive() {
 
 						std::cout << " " << std::setfill(' ');
 
+						// bid px
 						for (auto i = 4; i >= 0; i--) {
-							std::cout << (md.bid_px[i] != priv.bid_px[i] ? "[" : " ");
+							// std::cout << (f ? "[" : " ");
 							std::cout << std::setw(7) << std::fixed << std::setprecision(2) << (double)md.bid_px[i]/10000 << " ";
-							std::cout << (md.bid_px[i] != priv.bid_px[i] ? "]" : " ");
-							priv.bid_px[i] = md.bid_px[i];
+							// std::cout << (f ? "]" : " ");
 						}
 
 						std::cout << "| ";
@@ -470,22 +472,30 @@ void interactive() {
 
 						std::cout << " | ";
 
+						// ask px
 						for (auto i = 0; i <= 4; i++) {
-							std::cout << (md.ask_px[i] != priv.ask_px[i] ? "[" : " ");
+							// std::cout << (md.ask_px[i] != priv.ask_px[i] ? "[" : " ");
 							std::cout << std::setw(7) << std::fixed << std::setprecision(2) << (double)md.ask_px[i]/10000 << " ";
-							std::cout << (md.ask_px[i] != priv.ask_px[i] ? "]" : " ");
-							priv.ask_px[i] = md.ask_px[i];
+							// std::cout << (md.ask_px[i] != priv.ask_px[i] ? "]" : " ");
 						}
 
 						std::cout << std::endl;
 
 						std::cout << std::setw(14) << " ";
 
+						// bid lt
 						for (auto i = 4; i >= 0; i--) {
-							std::cout << (md.bid_lt[i] != priv.bid_lt[i] ? "[" : " ");
+
+							auto px = md.bid_px[i];
+							auto lt = md.bid_lt[i];
+							bool f = false;
+							if (bid_pxlt_map.find(px) == bid_pxlt_map.end()) bid_pxlt_map[px] = 0;
+							if (lt != bid_pxlt_map[px]) f = true;
+							bid_pxlt_map[px] = lt;
+
+							std::cout << (f ? "[" : " ");
 							std::cout << std::setw(7) << md.bid_lt[i] << " ";
-							std::cout << (md.bid_lt[i] != priv.bid_lt[i] ? "]" : " ");
-							priv.bid_lt[i] = md.bid_lt[i];
+							std::cout << (f ? "]" : " ");
 						}
 
 						std::cout << "| ";
@@ -497,56 +507,23 @@ void interactive() {
 
 						std::cout << " | ";
 
+						// ask lt
 						for (auto i = 0; i <= 4; i++) {
-							std::cout << (md.ask_lt[i] != priv.ask_lt[i] ? "[" : " ");
+
+							auto px = md.ask_px[i];
+							auto lt = md.ask_lt[i];
+							bool f = false;
+							if (ask_pxlt_map.find(px) == ask_pxlt_map.end()) ask_pxlt_map[px] = 0;
+							if (lt != ask_pxlt_map[px]) f = true;
+							ask_pxlt_map[px] = lt;
+
+							std::cout << (f ? "[" : " ");
 							std::cout << std::setw(7) << md.ask_lt[i] << " ";
-							std::cout << (md.ask_lt[i] != priv.ask_lt[i] ? "]" : " ");
-							priv.ask_lt[i] = md.ask_lt[i];
+							std::cout << (f ? "]" : " ");
 						}
 
 						std::cout << std::endl;
 
-						/*
-						printf("%06d:%06d: %7.2f %7.2f %7.2f %7.2f %7.2f | ", \
-							md.match_time_sec, md.match_time_usec, \
-							(double)md.bid_px[4]/10000, \
-							(double)md.bid_px[3]/10000, \
-							(double)md.bid_px[2]/10000, \
-							(double)md.bid_px[1]/10000, \
-							(double)md.bid_px[0]/10000);
-
-						if (md.with_trade)
-							printf("%7.2f", (double)md.trade_px/10000);
-						else
-							printf("%7s", " ");
-
-						printf(" | %7.2f %7.2f %7.2f %7.2f %7.2f\n", \
-							(double)md.ask_px[0]/10000, \
-							(double)md.ask_px[1]/10000, \
-							(double)md.ask_px[2]/10000, \
-							(double)md.ask_px[3]/10000, \
-							(double)md.ask_px[4]/10000);
-
-						printf("%14s %7d %7d %7d %7d %7d | ", \
-							" ", \
-							md.bid_lt[4], \
-							md.bid_lt[3], \
-							md.bid_lt[2], \
-							md.bid_lt[1], \
-							md.bid_lt[0]);
-
-						if (md.with_trade)
-							printf("%7d", md.trade_lt);
-						else
-							printf("%7s", " ");
-
-						printf(" | %7d %7d %7d %7d %7d\n", \
-							md.ask_lt[0], \
-							md.ask_lt[1], \
-							md.ask_lt[2], \
-							md.ask_lt[3], \
-							md.ask_lt[4]);
-						*/
 
 						getchar();
 					}
